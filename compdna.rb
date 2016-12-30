ecd8a28bacfe845b573d8ac3ea0080a8ae1dc3f1
Franky Liang <fliangz96@gmail.com>
2016-12-30 18:12:42 +0000

    Add notes about tr

--- compdna.rb
+++ compdna.rb
@@ -9,3 +9,15 @@ def DNA_strand(dna)
   n = n.join("")
   return n
 end
+
+# best solution
+
+def DNA_strand(dna)
+  dna.tr('ATCG','TAGC')
+end
+
+#tr(from_str, to_str) => new_str
+#Returns a copy of str with the characters in from_str replaced by the corresponding characters in to_str. If to_str is shorter than from_str, it is padded with its last character in order to maintain the correspondence.
+
+"hello".tr('el', 'ip')      #=> "hippo"
+"hello".tr('aeiou', '*')    #=> "h*ll*"
