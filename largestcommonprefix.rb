6cf92130a6377e56cd35c66a9b20ba0fbe44299a
Franky Liang <fliangz96@gmail.com>
2017-01-06 21:31:11 +0000

    Longest common prefix

--- largestcommonprefix.rb
+++ largestcommonprefix.rb
@@ -3,23 +3,30 @@ def lcp(a)
     prefix = ""
     shortest_string.each_char.with_index do |c, i|
         prefix = shortest_string[0..i]
-        puts "CURRENT PREFIX " + prefix
         should_break = false
         a.each do |ele|
             if !ele.index(prefix)
                 should_break = true
-                prefix = shortest_string[0..i-1]
+                if i == 0
+                    prefix = ""
+                else
+                    prefix = shortest_string[0..i-1]
+                end
             end
         end
         break if should_break
     end
     puts prefix + "  THIS IS PREFIX"
-    puts shortest_string
 end
 
 # Given array a
 # As an example, longest common prefix of "abcdefgh" and "abcefgh" is "abc".
 # Given the array of strings, you need to find the longest S which is the prefix of ALL the strings in the array.
 a = ["abcdefgh", "abaghijk", "abcefgh"]
+b = ["abcd", "abcd", "efgh"]
+c = ["abaacd", "abade", "abawcf"]
 lcp(a)
-
+puts "-----------------"
+lcp(b)
+puts "----------------"
+lcp(c)
