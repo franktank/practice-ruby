843dd6c9a0c1af71ff56d0d29066db7745b7960c
Franky Liang <fliangz96@gmail.com>
2017-01-07 01:45:25 +0000

    Add comment to Count and Say method

--- countandsay.rb
+++ countandsay.rb
@@ -1,16 +1,15 @@
 def cas(count_this)
     return "" if count_this == 0
     return "1" if count_this == 1
-    return "11" if count_this == 2
-    
-    prev = "11"
+
+    prev = "1"
     current = prev
-    for i in 3..count_this
-        prev += '@'
+    for i in 2..count_this # Loop to find item in the sequence
+        prev += '@' # Need this for extra iteration to check last item, @ is a dummy character
         current = ""
         count = 1
-        for j in 1..prev.length
-            if prev[j] != prev[j-1] && j < prev.length
+        for j in 1..prev.length # Loop through entire previous string to build next string in sequence
+            if prev[j] != prev[j-1] && j < prev.length # Dont count the last one (@), check to see if nonmatch is found
                 current += "#{count}"
                 current += "#{prev[j-1]}"
                 count = 1
@@ -21,10 +20,9 @@ def cas(count_this)
         prev = current
     end
     current
-    puts prev
 end
 # 1211 becomes 111221
 # 21 becomes one two, one one -> 1211
 # Generate 1 to n terms
 
-cas(3)
+puts cas(3)
