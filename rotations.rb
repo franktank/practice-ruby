0c087518d98dab871db2f6a1734df22d342fa057
Franky Liang <fliangz96@gmail.com>
2017-01-26 14:48:34 +0000

    WIP

--- rotations.rb
+++ rotations.rb
@@ -1,3 +1,3 @@
 def all_rotations(str, arr)
-    (0..str.length).all? { |i| arr.include?(str.chars.rotate(x).join) } # Rotate for array, check all possible rotations and see if include
+    (0..str.length).all? { |i| arr.include?(str.chars.rotate(x).join) } # Rotate string for array, check all possible rotations and see if included in array
 end
